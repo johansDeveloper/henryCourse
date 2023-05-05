6eb90aa15b3a0fff5962efb49bb0fa85ce5c1666
JohansErudiraptor <johans1023@hotmail.com>
2023-05-05 17:06:25 -0500

    Add files via upload

---
+++ main6.js
@@ -0,0 +1,27 @@
+
+
+function mayuscula(nombre){
+        
+    return nombre[0].toUpperCase() + nombre.slice(1);
+}
+
+function invocarCallBack(cb){
+    cb();
+}
+
+function operacionMatematica(n1, n2, cb){
+    return cb(n1, n2);
+}
+
+function sumarArray(){
+
+}
+
+function forEach(array, cb){
+    
+}
+
+
+
+
+
